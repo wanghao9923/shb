db6796be7817d151bf669dfbd53fb283d30c1c78
wanghao <m15075182791@163.com>
2018-04-10 12:28:21 +0800

    用户名

--- shb_demo/src/main/java/com/shb_demo/User.java
+++ shb_demo/src/main/java/com/shb_demo/User.java
@@ -1,5 +1,15 @@
 package com.shb_demo;
 
 public class User {
+	
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 }
