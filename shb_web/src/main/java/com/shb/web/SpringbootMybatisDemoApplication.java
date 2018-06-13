90404d3135fff6f108a2d648ab9ba6e77e1ffbce
wanghao <m15075182791@163.com>
2018-06-13 10:14:15 +0800

    web首次提交

---
+++ shb_web/src/main/java/com/shb/web/SpringbootMybatisDemoApplication.java
@@ -0,0 +1,25 @@
+package com.shb.web;
+
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+@SpringBootApplication
+//@MapperScan("com.shb.web.dao")
+public class SpringbootMybatisDemoApplication extends SpringBootServletInitializer {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootMybatisDemoApplication.class);
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisDemoApplication.class, args);
+	}
+	
+	
+}    
+
